b8936adfb8be24cf3e8f381afc1d7c3cb7223f5a
Efim Bryliakov <efim.bryliakov@devexpress.com>
2025-12-08 19:35:14 +0400

    Update custom view item example to follow modern best practices
    Simplified the ButtonDetailViewItemBlazor implementation:
    - ViewItem now implements IComponentContentHolder directly
    (no wrapper class needed)
    - Component model is created with all parameters in CreateControlCore
    
    This approach reduces boilerplate code and better demonstrates the
    recommended pattern for creating custom view items in XAF Blazor
    applications.

--- CS/EFCore/CustomViewItem/CustomViewItem.Blazor.Server/Editors/ButtonViewItem/ButtonDetailViewItemBlazor.cs
+++ CS/EFCore/CustomViewItem/CustomViewItem.Blazor.Server/Editors/ButtonViewItem/ButtonDetailViewItemBlazor.cs
@@ -1,45 +1,39 @@
-﻿using System;
-using DevExpress.ExpressApp;
+﻿using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Blazor.Components;
+using DevExpress.ExpressApp.Blazor.Components.Models;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using Microsoft.AspNetCore.Components;
 
-using DevExpress.ExpressApp.Blazor.Components;
+namespace CustomViewItem.Blazor.Server.Editors.ButtonViewItem;
 
-namespace MySolution.Module.Blazor {
-    public interface IModelButtonDetailViewItemBlazor : IModelViewItem { }
+public interface IModelButtonDetailViewItemBlazor : IModelViewItem;
 
-    [ViewItem(typeof(IModelButtonDetailViewItemBlazor))]
-    public class ButtonDetailViewItemBlazor : ViewItem, IComplexViewItem {
-        public class ButtonHolder : IComponentContentHolder {
-            public ButtonHolder(ButtonModel componentModel) {
-                ComponentModel = componentModel;
-            }
-            public ButtonModel ComponentModel { get; }
-            RenderFragment IComponentContentHolder.ComponentContent => ComponentModelObserver.Create(ComponentModel, ButtonRenderer.Create(ComponentModel));
-        }
-        private XafApplication application;
-        public ButtonDetailViewItemBlazor(IModelViewItem model, Type objectType) : base(objectType, model.Id) { }
-        void IComplexViewItem.Setup(IObjectSpace objectSpace, XafApplication application) {
-            this.application = application;
-        }
-        protected override object CreateControlCore() => new ButtonHolder(new ButtonModel());
-        protected override void OnControlCreated() {
-            if (Control is ButtonHolder holder) {
-                holder.ComponentModel.Text = "Click me!";
-                holder.ComponentModel.Click += ComponentModel_Click;
-            }
-            base.OnControlCreated();
-        }
-        public override void BreakLinksToControl(bool unwireEventsOnly) {
-            if (Control is ButtonHolder holder) {
-                holder.ComponentModel.Click -= ComponentModel_Click;
-            }
-            base.BreakLinksToControl(unwireEventsOnly);
-        }
-        private void ComponentModel_Click(object sender, EventArgs e) {
-            application.ShowViewStrategy.ShowMessage("Action is executed!");
-        }
+[ViewItem(typeof(IModelButtonDetailViewItemBlazor))]
+public class ButtonDetailViewItemBlazor(IModelViewItem model, Type objectType) : 
+    ViewItem(objectType, model.Id), 
+    IComponentContentHolder, 
+    IComplexViewItem
+{
+    private ButtonModel componentModel;
+    private XafApplication application;
+
+    RenderFragment IComponentContentHolder.ComponentContent =>
+        ComponentModelObserver.Create(componentModel, componentModel.GetComponentContent());
+    void IComplexViewItem.Setup(IObjectSpace objectSpace, XafApplication application) {
+        this.application = application;
+    }
+
+    protected override object CreateControlCore() {
+        componentModel = new ButtonModel
+        {
+            Text = "Click me!",
+            Click = EventCallback.Factory.Create(this, ComponentModel_Click),
+        };
+        return componentModel;
+    }
+    private void ComponentModel_Click() {
+        application.ShowViewStrategy.ShowMessage("Action is executed!");
     }
 }
--- CS/EFCore/CustomViewItem/CustomViewItem.Blazor.Server/Editors/ButtonViewItem/ButtonModel.cs
+++ CS/EFCore/CustomViewItem/CustomViewItem.Blazor.Server/Editors/ButtonViewItem/ButtonModel.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using DevExpress.ExpressApp.Blazor.Components.Models;
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using Microsoft.AspNetCore.Components;
 
-namespace MySolution.Module.Blazor {
-    public class ButtonModel : ComponentModelBase {
-        public string Text {
-            get => GetPropertyValue<string>();
-            set => SetPropertyValue(value);
-        }
-        public void ClickFromUI() {
-            Click?.Invoke(this, EventArgs.Empty);
-        }
-        public event EventHandler Click;
+namespace CustomViewItem.Blazor.Server.Editors.ButtonViewItem;
+
+public class ButtonModel : ComponentModelBase {
+    public string Text {
+        get => GetPropertyValue<string>();
+        set => SetPropertyValue(value);
     }
+    public EventCallback Click {
+        get => GetPropertyValue<EventCallback>();
+        set => SetPropertyValue(value);
+    }
+
+    public override Type ComponentType => typeof(Button);
 }
